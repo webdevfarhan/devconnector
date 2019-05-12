68ab152ee2e9a1bd3cd3086eb955861e61ddf281
Farhan <farhan@nexgin.com>
2019-05-12 23:46:59 +0530

    add middleware for private routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 /*
@@ -52,8 +54,23 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
-      // Return jsonwebtoken
-      res.send('User Registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server error');
