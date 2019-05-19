e0f80982e03a0f4ab8bfe3dc9c6220d5a3e0070f
Farhan <farhan@nexgin.com>
2019-05-19 17:23:57 +0530

    add get all profiles and find profile by Id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -93,4 +93,41 @@ router.post(
   }
 );
 
+/*
+@route GET api/profile/
+@desc Get all profiles
+@access Public
+*/
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+/*
+@route GET api/profile/user/:user_id
+@desc Get profile by user id
+@access Public
+*/
+
+router.get('/user/:user_id', async (req, res) => {
+  console.log('gone');
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
