d7a54b9afe1ea6abc74f01ceed53279e0e046cac
Farhan <farhan@nexgin.com>
2019-05-21 23:01:02 +0530

    create post model

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Data.now()
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Data.now()
+  }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
