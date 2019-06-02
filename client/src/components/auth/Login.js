46ea47523dea3b9a3e431fcf31278c22ddfa26a0
Farhan <farhan@nexgin.com>
2019-06-02 17:28:44 +0530

    logout and navbar links

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -20,27 +20,27 @@ const Login = ({ login, isAuthenticated }) => {
 
   //Redirect if logged in
   if (isAuthenticated) {
-    return <Redirect to="/dashboard" />;
+    return <Redirect to='/dashboard' />;
   }
 
   return (
     <Fragment>
-      <h1 className="large text-primary">Sign In</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Sign into Your Account
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Sign into Your Account
       </p>
-      <form className="form" onSubmit={e => onSubmit(e)}>
-        <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
-          <small className="form-text">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input type='email' placeholder='Email Address' name='email' value={email} onChange={e => onChange(e)} />
+          <small className='form-text'>This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
         </div>
-        <div className="form-group">
-          <input type="password" placeholder="Password" name="password" minLength="6" value={password} onChange={e => onChange(e)} />
+        <div className='form-group'>
+          <input type='password' placeholder='Password' name='password' minLength='6' value={password} onChange={e => onChange(e)} />
         </div>
-        <input type="submit" className="btn btn-primary" value="Register" />
+        <input type='submit' className='btn btn-primary' value='Login' />
       </form>
-      <p className="my-1">
-        Don't have an account? <Link to="register">Sign Up</Link>
+      <p className='my-1'>
+        Don't have an account? <Link to='register'>Sign Up</Link>
       </p>
     </Fragment>
   );
