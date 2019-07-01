6d43995440888f9ac3566d60de6367aa2b76dff5
Farhan <frhnmdni@gmail.com>
2019-07-01 18:47:13 +0530

    adding posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from '../actions/alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST } from './types';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -52,7 +52,6 @@ export const removeLike = postId => async dispatch => {
 
 // Delete post
 export const deletePost = id => async dispatch => {
-  console.log('gone');
   try {
     await axios.delete(`/api/posts/${id}`);
     dispatch({
@@ -61,7 +60,45 @@ export const deletePost = id => async dispatch => {
     });
     dispatch(setAlert('Post removed', 'success'));
   } catch (err) {
-    console.log('catch block');
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  try {
+    const res = await axios.post('/api/posts/', formData, config);
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+    dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get Single Post
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
+  } catch (err) {
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -8,10 +8,10 @@ import { addLike, removeLike, deletePost } from '../../actions/post';
 const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }, addLike, removeLike, deletePost }) => (
   <div className='post bg-white p-1 my-1'>
     <div>
-      <a href='profile.html'>
+      <Link to={`/profile/${user}`}>
         <img className='round-img' src={avatar} alt='' />
         <h4>{name}</h4>
-      </a>
+      </Link>
     </div>
     <div>
       <p className='my-1'>{text}</p>
