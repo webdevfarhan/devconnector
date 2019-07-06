3f428e0a72b3a0e640374f324623f760e94954a0
Farhan <frhnmdni@gmail.com>
2019-07-06 14:27:47 +0530

    deployment

--- server.js
+++ server.js
@@ -10,16 +10,22 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
